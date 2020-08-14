3246f1763403f08fcf9b48d85ee020560b10b870
Claire Rioualen <rioualenclaire@gmail.com>
2020-08-14 13:34:57 -0500

    added tf conversion function and source file

--- R/read_master_gene_file.R
+++ R/read_master_gene_file.R
@@ -7,7 +7,7 @@
 #'
 #' @examples
 read_master_gene_file <- function() {
-	master_gene_file <- system.file("extdata", "GeneProduct-IDs.tsv", package = "EcoliGenes")
+	master_gene_file <- system.file("extdata", "master_gene_file.tsv", package = "EcoliGenes")
 	master_gene_table <- read.delim(master_gene_file, comment.char = "#", header = T, stringsAsFactors = F)
 	master_gene_table
 }
---
+++ R/tf_to_bnumber.R
@@ -0,0 +1,24 @@
+#' @title Convert TF protein names into bnumbers
+#' @name tf_to_bnumber
+#'
+#' @param list_proteins A character vector
+#'
+#' @return A character vector of same size as `list_proteins`
+#'
+#' @import tidyr
+#' @export
+#'
+#' @examples
+tf_to_bnumber <- function(list_proteins) {
+	master_tf_table <- read_master_tf_file()
+
+	tf_list_by_synonyms <- master_tf_table %>% tidyr::separate_rows(PROTEIN.SYNONIMS.ECO.v23, sep = ",")
+	tf_list_by_synonyms <- split(tf_list_by_synonyms, tf_list_by_synonyms$PROTEIN.SYNONIMS.ECO.v23)
+
+	convert_proteins <- function(x) {
+		ifelse(!is.null(gene_list_by_symbol_or_synonym[[x]]$BNUMBER.ECO.v23[1]), "", warning(paste0("This protein name is unknown and will be converted to `FALSE`: ", x), call. = FALSE))
+		ifelse(!is.null(gene_list_by_symbol_or_synonym[[x]]$BNUMBER.ECO.v23[1]), tf_list_by_synonyms[[x]]$BNUMBER.ECO.v23[1], FALSE)
+	}
+	list_bnumbers <- sapply(list_proteins, FUN=convert_proteins)
+	unname(list_bnumbers)
+}
