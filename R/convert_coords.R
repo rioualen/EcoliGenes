854c4372bf6fb0767b273c957d588878f44a82ff
Claire Rioualen <rioualenclaire@gmail.com>
2022-04-28 15:00:50 -0500

    new function to convert obsolete gene coordinates

---
+++ R/convert_coords.R
@@ -0,0 +1,20 @@
+#' @title Convert coordinates from 96.2 to 96.3
+#' @name convert_coords
+#'
+#' @param coords A integer vector
+#'
+#' @return A integer vector of same size as `coords`
+#'
+#' @export
+#'
+#' @examples
+convert_coords <- function(coords) {
+
+	coords_df <- read_coords_file()
+	coords_list <- split(coords_df$v3, coords_df$v2)
+
+	convert <- function(x) {
+		coords_list[[as.character(x)]]
+	}
+	mapply(coords, FUN = convert)
+}
