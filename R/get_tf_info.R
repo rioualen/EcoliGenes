c62f41dc80e0e72a3af439b85f9ddcb6f7f3d92a
Claire Rioualen <rioualenclaire@gmail.com>
2022-07-15 17:53:26 -0500

    added functions to get TF attributes such as symmetry, effect, family

--- R/get_tf_info.R
+++ R/get_tf_info.R
@@ -1,51 +1,96 @@
-#' @title Get TF family
+#' @title Get family info for a given TF or list of TFs
 #' @name get_tf_family
-#'
 #' @param list_tfs
-#'
 #' @return A character vector of same size as `list_tfs`
-#'
 #' @export
-#'
 #' @examples
 get_tf_family <- function(list_tfs) {
-	master_tf_table <- read_master_tf_file()
+	master_tf_table <- read_source_file("tfs")
 
-	tf_list_by_bnum <- split(master_tf_table, master_tf_table$Reference_bnumber)
+	tf_list_by_name <- split(master_tf_table, master_tf_table$Reference_name)
 
 	get_fam <- function(x) {
-		ifelse(!is.null(tf_list_by_bnum[[x]]$RegulonDB_tf_family[1]), tf_list_by_bnum[[x]]$RegulonDB_tf_family[1], NA)
+		ifelse(!is_tf(x), warning("Unknown TF '", x, "'; try to use convert_tf('", x, "', to = 'name') first."), NA)
+		ifelse(!is.null(tf_list_by_name[[x]]$RegulonDB_tf_family[1]), tf_list_by_name[[x]]$RegulonDB_tf_family[1], NA)
 	}
-	list_fam <- sapply(list_tfs, FUN=get_fam)
+	list_fam <- sapply(list_tfs, FUN = get_fam)
 	unname(list_fam)
 }
 
-
 #' @title Get tf synonyms from the master table for a given vector of tfs
 #' @name get_tf_synonyms
-#'
 #' @param list_tfs A character vector of tf names
-#'
 #' @return A character vector of same size as `list_tfs` with comma-separated synonyms
-
-#'
 #' @import dplyr
 #' @export
-#'
 #' @examples
 get_tf_synonyms <- function(list_tfs) {
 	master_tf_table <- read_source_file("tfs")
 
-	tf_list_by_bnum <- split(master_tf_table, master_tf_table$Reference_name)
-
-	# ## Check that provided list of genes are valid bnumbers, or can be converted into them
-	# list_bnum <- get_gene_bnumber(list_genes)
+	tf_list_by_name <- split(master_tf_table, master_tf_table$Reference_name)
 
 	tf_synonyms <- function(x) {
-		ifelse(!is.na(x), gene_list_by_bnum[[x]]$protein_synonyms, NA)
+		ifelse(!is_tf(x), warning("Unknown TF '", x, "'; try to use convert_tf('", x, "', to = 'name') first."), NA)
+		ifelse(!is.null(tf_list_by_name[[x]]$TF_synonyms), tf_list_by_name[[x]]$TF_synonyms, NA)
 	}
 	list_synonyms <- sapply(list_tfs, FUN = tf_synonyms)
 	unname(list_synonyms)
 }
 
+#' @title Get symmetry info for a given TF or list of TFs
+#' @name get_tf_symmetry
+#' @param list_tfs
+#' @return A character vector of same size as `list_tfs`
+#' @export
+#' @examples
+get_tf_symmetry <- function(list_tfs) {
+	master_tf_table <- read_source_file("tfs")
+
+	tf_list_by_name <- split(master_tf_table, master_tf_table$Reference_name)
+
+	get_sym <- function(x) {
+		ifelse(!is_tf(x), warning("Unknown TF '", x, "'; try to use convert_tf('", x, "', to = 'name') first."), NA)
+		ifelse(!is.null(tf_list_by_name[[x]]$RegulonDB_tf_symmetry[1]), tf_list_by_name[[x]]$RegulonDB_tf_symmetry[1], NA)
+	}
+	list_sym <- sapply(list_tfs, FUN = get_sym)
+	unname(list_sym)
+}
+
+#' @title Get class info for a given TF or list of TFs
+#' @name get_tf_class
+#' @param list_tfs
+#' @return A character vector of same size as `list_tfs`
+#' @export
+#' @examples
+get_tf_class <- function(list_tfs) {
+	master_tf_table <- read_source_file("tfs")
+
+	tf_list_by_name <- split(master_tf_table, master_tf_table$Reference_name)
+
+	get_class <- function(x) {
+		ifelse(!is_tf(x), warning("Unknown TF '", x, "'; try to use convert_tf('", x, "', to = 'name') first."), NA)
+		ifelse(!is.null(tf_list_by_name[[x]]$RegulonDB_tf_class[1]), tf_list_by_name[[x]]$RegulonDB_tf_class[1], NA)
+	}
+	list_class <- sapply(list_tfs, FUN = get_class)
+	unname(list_class)
+}
+
+#' @title Get conformation_type info for a given TF or list of TFs
+#' @name get_tf_effect
+#' @param list_tfs
+#' @return A character vector of same size as `list_tfs`
+#' @export
+#' @examples
+get_tf_effect <- function(list_tfs) {
+	master_tf_table <- read_source_file("tfs")
+
+	tf_list_by_name <- split(master_tf_table, master_tf_table$Reference_name)
+
+	get_effect <- function(x) {
+		ifelse(!is_tf(x), warning("Unknown TF '", x, "'; try to use convert_tf('", x, "', to = 'name') first."), NA)
+		ifelse(!is.null(tf_list_by_name[[x]]$RegulonDB_tf_conformation_type[1]), tf_list_by_name[[x]]$RegulonDB_tf_conformation_type[1], NA)
+	}
+	list_effect <- sapply(list_tfs, FUN = get_effect)
+	unname(list_effect)
+}
 
