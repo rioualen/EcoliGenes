cef59f9315c3955f0b09de28af93ca09facf870f
Claire Rioualen <rioualenclaire@gmail.com>
2022-07-15 17:57:10 -0500

    update call to read_source function

--- R/convert_coords.R
+++ R/convert_coords.R
@@ -1,4 +1,4 @@
-#' @title Convert coordinates from 96.2 to 96.3
+#' @title Convert genomic coordinates from 96.2 to 96.3
 #' @name convert_coords
 #'
 #' @param coords A integer vector
@@ -10,7 +10,7 @@
 #' @examples
 convert_coords <- function(coords) {
 
-	coords_df <- read_coords_file()
+	coords_df <- read_source_file("coords")
 	coords_list <- split(coords_df$v3, coords_df$v2)
 
 	convert <- function(x) {
