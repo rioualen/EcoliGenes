f3aed75b5379b27468e1a5041d0cb2edcf9c51c8
Claire Rioualen <rioualenclaire@gmail.com>
2022-07-15 17:47:14 -0500

    renamed functions, refactored code, updated description

--- R/test_position.R
+++ R/test_position.R
@@ -1,14 +1,10 @@
 #' @title Check if a position or vector of positions are genic
 #' @name is_genic
-#'
-#' @param positions A numeric vector
-#' @param strand A character vector
-#'
-#' @return A vector of logicals of size `length(positions)`
-#'
+#' @param positions A vector of integers
+#' @param strand A character vector of size 1 or same size as `positions`
+#' @return A vector of logicals of same size as `positions`
 #' @import dplyr
 #' @export
-#'
 #' @examples
 is_genic <- function(positions, strand) {
 	master_gene_table <- read_master_gene_file()
@@ -24,100 +20,75 @@ is_genic <- function(positions, strand) {
 	mapply(FUN = check_genic, positions, strand)
 }
 
-#' @title Check in which gene
-#' @name where_genic
-#'
-#' @param positions A numeric vector
-#' @param strand A character vector
-#'
-#' @return A character vector of same size
-#'
+#' @title Check in which gene falls a given position
+#' @name is_genic_which
+#' @param positions A vector of integers
+#' @param strand A character vector of size 1 or same size as `positions`
+#' @return A character vector of same size as `positions`
 #' @import dplyr
 #' @export
-#'
 #' @examples
-where_genic <- function(positions, strand) {
+is_genic_which <- function(positions, strand) {
 	master_gene_table <- read_master_gene_file()
 
-		check_genic_which_gene <- function(x, y) {
-			genes <- master_gene_table %>% filter(Reference_strand == y)
-			lo <- x >= genes$Reference_start
-			hi <- x <= genes$Reference_stop
-			check <- lo * hi
-			df <- genes[which(check == 1),]
+	check_genic_which_gene <- function(x, y) {
+		genes <- master_gene_table %>% filter(Reference_strand == y)
+		lo <- x >= genes$Reference_start
+		hi <- x <= genes$Reference_stop
+		check <- lo * hi
+		df <- genes[which(check == 1),]
 		if(nrow(df) == 0 ){
 			NA
 		} else {
-			paste(df$Reference_symbol, collapse = ",")
+			paste(df$Reference_bnumber, collapse = ",")
 		}
 	}
-	list_genes <- sapply(positions, strand, FUN = check_genic_which_gene)
-	unname(list_genes)
+	mapply(positions, strand, FUN = check_genic_which_gene)
 }
 
 
-#' @title Check where in gene
-#' @name where_genic_rel
-#'
+#' @title Check where in gene (relative position from 0 to 1, 0 being the start codon position)
+#' @name is_genic_where
 #' @param positions A numeric vector
-#' @param strand A character vector
-#'
-#' @return A numeric vector of same size
-#'
-#' @import dplyr
+#' @param strand A character vector of same size as `positions`
+#' @return A numeric vector of same size as `positions`
 #' @import tidyr
+#' @import dplyr
 #' @export
-#'
 #' @examples
-where_genic_rel <- function(positions, strand) {
+is_genic_where <- function(positions, strand) {
 
 	# master_gene_table <- read_master_gene_file()
 
-	check_genic_with_pos <- function(x, y) {
-		genes_sym <- where_genic(x, y)
-		df <- data.frame(x, y, genes_sym) %>%
-			tidyr::separate_rows(genes_sym, sep = ",") %>%
-			dplyr::mutate(
-				genes_bnum = get_gene_bnumber(genes_sym),
-				gene_start = get_gene_start(genes_bnum),
-				gene_stop = get_gene_stop(genes_bnum),
-				relative_pos = ifelse(y == "-", round((gene_stop - x)/(gene_stop - gene_start), 2),
-															 ifelse(y == "+", round((x - gene_start)/(gene_stop - gene_start), 2), NA))
-			) %>%
-			dplyr::group_by(x, y)
-		paste(df$relative_pos, collapse = ",")
+	check_genic_with_rel_pos <- function(x, y) {
+		gene <- where_genic(x, y)
 
-		# genes <- master_gene_table %>% filter(Reference_strand == y)
-		# lo <- x >= genes$Reference_start
-		# hi <- x <= genes$Reference_stop
-		# check <- lo * hi
-		# df <- genes[which(check == 1),]
-		# if(nrow(df) == 0 ){
-		# 	NA
-		# } else {
-		# 	paste(df$Reference_bnumber, collapse = ",")
-		# }
+		if(!is.na(gene)){
+			gene_bnum <- get_gene_bnumber(gene)
+			gene_start <- get_gene_start(gene_bnum)
+			gene_stop <- get_gene_stop(gene_bnum)
+			relative_pos <- ifelse(y == "-", round((gene_stop - x)/(gene_stop - gene_start), 2),
+														ifelse(y == "+", round((x - gene_start)/(gene_stop - gene_start), 2), NA))
+		} else {
+			relative_pos <- NA
+		}
+		relative_pos
 	}
-	list_relpos <- sapply(positions, strand, FUN = check_genic_with_pos)
-	unname(list_relpos)
+	mapply(positions, strand, FUN = check_genic_with_rel_pos)
 }
 
-
-#' @title Get closest gene bnumber
+#' @title Get closest genes from genomic positions given a strand
 #' @name closest_gene
-#'
-#' @param position A numeric vector
-#' @param strand A character in c("+", "-", "+-")
-#'
-#' @return A vector of characters of same size as input
-#'
-#' @import dplyr
+#' @param positions A vector of integers
+#' @param strand A character vector of size 1 or same size as `positions`
+#' @return A character vector of same size as `positions`
 #' @import stringr
+#' @import dplyr
 #' @export
-#'
 #' @examples
-closest_gene <- function(position, strand = "+-") {
-	master_gene_table <- read_master_gene_file()
+closest_gene <- function(positions, strand = "+-") {
+	master_gene_table <- read_master_gene_file() %>%
+		dplyr::mutate(aug = ifelse(Reference_strand == "+", Reference_start, ifelse(Reference_strand == "-", Reference_stop, NA)))
 
 	get_closest <- function(x, y) {
 		strd <- c(stringr::str_split(y, "", simplify = T))
@@ -126,6 +97,6 @@ closest_gene <- function(position, strand = "+-") {
 			dplyr::filter(abs(x - aug) == min(abs(x - df$aug)))
 		gene$Reference_bnumber[1] ## OJO there can be more than one answer!
 	}
-	mapply(position, strand, FUN = get_closest)
+	mapply(positions, strand, FUN = get_closest)
 }
 
